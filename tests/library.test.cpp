37a0bc6cbbd8b156994e752e802f002ab24bc063
Khalil Estell <kammcecorp@gmail.com>
2023-01-30 06:47:56 -0800

    :recycle: Conform to the current libhal standard

---
+++ tests/library.test.cpp
@@ -0,0 +1,17 @@
+#include <libhal-library/library.hpp>
+
+#include <boost/ut.hpp>
+
+namespace hal::library {
+void library_test()
+{
+  using namespace boost::ut;
+  using namespace std::literals;
+
+  "library::create()"_test = []() {
+    // Setup
+    // Exercise
+    // Verify
+  };
+};
+}  // namespace hal::library
