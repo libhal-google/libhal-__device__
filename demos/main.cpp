fdea277f23e0f2e879e7ec70d94de17a2a303b83
Khalil Estell <kammce@google.com>
2023-06-25 15:34:46 -0700

    :recycle: Migrate to prebuilt

--- demos/main.cpp
+++ demos/main.cpp
@@ -14,15 +14,24 @@
 
 #include "hardware_map.hpp"
 
+#include <sys/stat.h>
+#include <unistd.h>
+
 int main()
 {
-  auto init_result = initialize_target();
+  auto processor_status = initialize_processor();
 
-  if (!init_result) {
+  if (!processor_status) {
     hal::halt();
   }
 
-  auto hardware_map = init_result.value();
+  auto platform_status = initialize_platform();
+
+  if (!platform_status) {
+    hal::halt();
+  }
+
+  auto hardware_map = platform_status.value();
   auto is_finished = application(hardware_map);
 
   if (!is_finished) {
@@ -35,8 +44,65 @@ int main()
 }
 
 namespace boost {
-void throw_exception(std::exception const& e)
+void throw_exception([[maybe_unused]] std::exception const& e)
 {
   std::abort();
 }
 }  // namespace boost
+
+extern "C"
+{
+  /// Dummy implementation of getpid
+  int _getpid_r()
+  {
+    return 1;
+  }
+
+  /// Dummy implementation of kill
+  int _kill_r(int, int)
+  {
+    return -1;
+  }
+
+  /// Dummy implementation of fstat, makes the assumption that the "device"
+  /// representing, in this case STDIN, STDOUT, and STDERR as character devices.
+  int _fstat_r([[maybe_unused]] int file, struct stat* status)
+  {
+    status->st_mode = S_IFCHR;
+    return 0;
+  }
+
+  int _write_r([[maybe_unused]] int file,
+               [[maybe_unused]] const char* ptr,
+               int length)
+  {
+    return length;
+  }
+
+  int _read_r([[maybe_unused]] FILE* file,
+              [[maybe_unused]] char* ptr,
+              int length)
+  {
+    return length;
+  }
+
+  // Dummy implementation of _lseek
+  int _lseek_r([[maybe_unused]] int file,
+               [[maybe_unused]] int ptr,
+               [[maybe_unused]] int dir)
+  {
+    return 0;
+  }
+
+  // Dummy implementation of close
+  int _close_r([[maybe_unused]] int file)
+  {
+    return -1;
+  }
+
+  // Dummy implementation of isatty
+  int _isatty_r([[maybe_unused]] int file)
+  {
+    return 1;
+  }
+}
---
+++ src/__device__.cpp
@@ -0,0 +1,3 @@
+#include "libhal-__device__/__device__.hpp"
+
+// Add code implementation here!
