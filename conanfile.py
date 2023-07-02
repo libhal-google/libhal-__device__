cc5442fa9793cd5dc2de4af87092a15457fe0198
Khalil Estell <kammce@google.com>
2023-07-02 11:19:50 -0700

    :truck: Rename targets -> platform in demos

--- conanfile.py
+++ conanfile.py
@@ -58,12 +58,12 @@ class libhal___device___conan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("libhal-cmake-util/1.0.0")
-        self.build_requires("libhal-util/[^2.0.0]")
         self.test_requires("libhal-mock/[^2.0.0]")
         self.test_requires("boost-ext-ut/1.1.9")
 
     def requirements(self):
         self.requires("libhal/[^2.0.0]")
+        self.requires("libhal-util/[^2.0.0]")
 
     def layout(self):
         cmake_layout(self)
